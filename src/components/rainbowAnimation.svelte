11c9b5c70d1897bac1e70048ff7b3a9b5010b1e0
Krister Torsvik <krister.torsvik@itera.com>
2023-08-23 09:40:21 +0200

    make animation smoother

--- src/components/rainbowAnimation.svelte
+++ src/components/rainbowAnimation.svelte
@@ -3,23 +3,23 @@
 </script>
 
 <div class="💪">
-    <DiagonalRow --color="red" --start=100% --stop=0% />
-    <DiagonalRow --color="orange" --start=100% --stop=0% />
-    <DiagonalRow --color="yellow" --start=100% --stop=0% />
-    <DiagonalRow --color="green" --start=100% --stop=0% />
-    <DiagonalRow --color="blue" --start=100% --stop=0% />
-    <DiagonalRow --color="indigo" --start=100% --stop=0% />
-    <DiagonalRow --color="violet" --start=100% --stop=0% />
+    <DiagonalRow --color="red" --start=105% --stop=0% />
+    <DiagonalRow --color="orange" --start=105% --stop=0% />
+    <DiagonalRow --color="yellow" --start=105% --stop=0% />
+    <DiagonalRow --color="green" --start=105% --stop=0% />
+    <DiagonalRow --color="blue" --start=105% --stop=0% />
+    <DiagonalRow --color="indigo" --start=105% --stop=0% />
+    <DiagonalRow --color="violet" --start=105% --stop=0% />
 </div>
 
 <div class="💪 overlay">
-    <DiagonalRow --color="red" --start=0% --stop=100% --degree="45deg" />
-    <DiagonalRow --color="orange" --start=0% --stop=100% --degree="45deg" />
-    <DiagonalRow --color="yellow" --start=0% --stop=100% --degree="45deg" />
-    <DiagonalRow --color="green" --start=0% --stop=100% --degree="45deg" />
-    <DiagonalRow --color="blue" --start=0% --stop=100% --degree="45deg" />
-    <DiagonalRow --color="indigo" --start=0% --stop=100% --degree="45deg" />
-    <DiagonalRow --color="violet" --start=0% --stop=100% --degree="45deg" />
+    <DiagonalRow --color="red" --start=0% --stop=105% --degree="45deg" />
+    <DiagonalRow --color="orange" --start=0% --stop=105% --degree="45deg" />
+    <DiagonalRow --color="yellow" --start=0% --stop=105% --degree="45deg" />
+    <DiagonalRow --color="green" --start=0% --stop=105% --degree="45deg" />
+    <DiagonalRow --color="blue" --start=0% --stop=105% --degree="45deg" />
+    <DiagonalRow --color="indigo" --start=0% --stop=105% --degree="45deg" />
+    <DiagonalRow --color="violet" --start=0% --stop=105% --degree="45deg" />
 </div>
 
 <style>
