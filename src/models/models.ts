64bab7e32aed19485263a24795bc53c725c94e47
Krister Torsvik <krister.torsvik@itera.com>
2023-08-29 15:12:49 +0200

    add cat from api

--- src/models/models.ts
+++ src/models/models.ts
@@ -17,8 +17,13 @@ export interface YCoordinates {
 export interface CatImageObject {
 	id: number;
 	src: string;
+	url: string;
 	liked: boolean;
 	description: string;
+}
+
+export interface CatImageWithBreedDetailsObject extends CatImageObject {
+	breed: {weight: { imperial: string, meteric: string}, id: string, name: string, temperament: string, origin: string, country_codes: string, country_code: string, life_span: string, wikipedia_url: string }[]
 }
 
 export interface StoreCatImages {
